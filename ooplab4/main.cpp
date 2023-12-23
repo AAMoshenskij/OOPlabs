67406430cb0c387615a42873cf717702667d0d75
AAMoshenskij <129053924+AAMoshenskij@users.noreply.github.com>
2023-12-24 00:36:33 +0300

    Add files via upload

---
+++ ooplab4/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <memory>
+#include "figure.h"
+#include "trapez.h"
+#include "rectangle.h"
+#include "square.h"
+#include "template_array.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::shared_ptr;
+using std::make_shared;
+
+int main() {    
+    
+    using vertex_t = std::pair<double, double>;
+
+    try {
+
+        shared_ptr<Figure<double> > trapez_ptr(make_shared<trapez<double>>(10, 0, 1, 4, 1, 0));
+        (*trapez_ptr) << cout;
+        
+        cout << trapez_ptr->figure_square_calc() << endl;
+        vertex_t geo = trapez_ptr->geo_center_calc();
+        cout << geo.first << " " << geo.second << endl;
+
+        Array<shared_ptr<Figure<double> > > array{
+                                    make_shared<Trapez<double>>(1, 2, 3, 0, 10, 5),
+                                    make_shared<Square<double>>(1, 1, 4, 4, 1, 4, 4, 1),
+                                    make_shared<Rectangle<double>>(10, 1, 15, 1, 15, 3, 10, 3),
+                                    make_shared<Trapez<double>>(0, 0, 5, 5, 3, 0)
+                                };
+        
+
+        for(size_t i = 0;i < array.size(); ++i) {
+            *(array[i]) << cout << endl;
+        }
+
+        Array<shared_ptr<Figure<double> > > array2 = array;
+
+        std::cout << "Print array2 ------------------------------" << endl;
+
+        double total_square = 0;
+        for(size_t i = 0; i < array2.size(); ++i) {
+            array2[i]->print(cout);
+            vertex_t cur_geo_center = array2[i]->geo_center_calc();
+            cout << "Geo_center:\n x: " << cur_geo_center.first << ", y: " <<  cur_geo_center.second << endl;
+            cout << "Current figure square = " << array2[i]->figure_square_calc();
+            cout << endl << endl;
+            total_square += array2[i]->figure_square_calc();
+        }
+
+        Array<shared_ptr<Figure<double>>> array3 = move(array2);
+        cout << "Print array3 ------------------------------" << endl;
+
+        for(size_t i = 0;i < array3.size(); ++i) {
+            // (*array3[i]) << cout << endl;
+            array3[i]->print(cout);
+            cout << endl;
+        }
+
+        size_t rm_idx;
+        cout << "Enter the index to remove the figure from array: ";
+        cin >> rm_idx;
+        
+        array.remove(rm_idx);
+        // cout << "Ok!" << endl;
+        for(size_t i = 0; i < array.size(); ++i) {
+            array[i]->print(cout);
+            cout << endl;
+        }
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
---
+++ ooplab4/trapez.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <iostream>
+#include "figure.h"
+#include <math.h>
+
+template<typename T>
+class Trapez: public Figure<T> {
+
+public:
+    using vertex_t = std::pair<T,T>;
+
+    std::istream& operator>>(std::istream& is) override{
+
+        std::cout << "Input the coordinates of the first point" << std::endl;
+        std::cout << "order [x1,y1], separated by spaces : ";
+
+        is >> v1.first >> v1.second;
+
+        std::cout << "Input the coordinates of the second point" << std::endl;
+        std::cout << "order [x2,y2], separated by spaces : ";
+
+        is >> v2.first >> v2.second;
+
+        std::cout << "Input the coordinates of the third point" << std::endl;
+        std::cout << "order [x3,y3], separated by spaces : ";
+
+        is >> v3.first >> v3.second;
+
+        T side1 = sqrt((v1.first - v2.first)*(v1.first - v2.first) + (v1.second - v2.second)*(v1.second - v2.second));
+        T side2 = sqrt((v1.first - v3.first)*(v1.first - v3.first) + (v1.second - v3.second)*(v1.second - v3.second));
+        T side3 = sqrt((v3.first - v2.first)*(v3.first - v2.first) + (v3.second - v2.second)*(v3.second - v2.second));
+
+        side1 = side1;
+        side2 = side2;
+        side3 = side3;
+
+        return is;
+    }
+
+    std::ostream& operator<<(std::ostream& os) override{
+        os << "Point1 [ x1 : " << v1.first << ", y1 : " << v1.second << " ]" << std::endl;
+        os << "Point2 [ x2 : " << v2.first << ", y2 : " << v2.second << " ]" << std::endl;
+        os << "Point3 [ x3 : " << v3.first << ", y3 : " << v3.second << " ]" << std::endl;
+
+        return os;
+    }
+
+    std::ostream& print(std::ostream& os) override {
+        *this << os;
+        return os;   
+    }
+
+    Trapez() {
+        v1.first = 0;
+        v1.second = 0;
+        v2.first = 0;
+        v2.second = 0;
+        v3.first = 0;
+        v3.second = 0;
+        side1 = 0;
+        side2 = 0;
+        side3 = 0;
+    }
+    Trapez(T _x1, T _y1, T _x2, T _y2, T _x3, T _y3) {
+        v1.first = _x1;
+        v1.second = _y1;
+        v2.first = _x2;
+        v2.second = _y2;
+        v3.first = _x3;
+        v3.second = _y3;
+
+
+        T side1 = sqrt((v1.first - v2.first)*(v1.first - v2.first) + (v1.second - v2.second)*(v1.second - v2.second));
+        T side2 = sqrt((v1.first - v3.first)*(v1.first - v3.first) + (v1.second - v3.second)*(v1.second - v3.second));
+        T side3 = sqrt((v3.first - v2.first)*(v3.first - v2.first) + (v3.second - v2.second)*(v3.second - v2.second));
+
+        this->side1 = side1 ;
+        this->side2 = side2;
+        this->side3 = side3;
+    }
+
+    operator double() {                     //приводит к double, когда необходимо
+        T sq = figure_square_calc();
+        return static_cast<double>(sq);
+    }
+
+    Trapez operator=(Trapez<T>& t) {
+
+        v1.first = t.v1.first;
+        v2.first = t.v2.first;
+        v3.first = t.v3.first;
+        v1.second = t.v1.second;
+        v2.second = t.v2.second;
+        v3.second = t.v3.second;
+        side1 = t.side1;
+        side2 = t.side2;
+        side3 = t.side3;
+        
+        return *this;
+    }
+
+    friend bool operator==(Trapez<T> t1, Trapez<T> t2) {
+        int result = 1;
+        result *= (t1.v1.first == t2.v1.first);
+        result *= (t1.v2.first == t2.v2.first);
+        result *= (t1.v3.first == t2.v3.first);
+        result *= (t1.v1.second == t2.v1.second);
+        result *= (t1.v2.second == t2.v2.second);
+        result *= (t1.v3.second == t2.v3.second);
+        return static_cast<bool>(result);
+    }
+
+    virtual T figure_square_calc() const override {
+        T half_meter = (side1 + side2 + side3) / 2;
+        T sq = sqrt(half_meter * (half_meter - side1) * (half_meter - side2) * (half_meter - side3));
+        return sq;
+    }
+
+    virtual vertex_t geo_center_calc() override {
+
+        vertex_t mid;
+
+        T mid_x = (v1.first + v2.first + v3.first) / 3;
+        T mid_y = (v1.second + v2.second + v3.second) / 3;
+        mid.first = mid_x;
+        mid.second = mid_y;
+        this->geo_center = mid;
+
+        return mid;
+    }
+
+    ~Trapez() = default;
+
+private:
+    // Coordinates
+    vertex_t v1;
+    vertex_t v2;
+    vertex_t v3;
+    vertex_t v4;
+
+    //Sides
+    T side1;
+    T side2;
+    T side3;
+};
